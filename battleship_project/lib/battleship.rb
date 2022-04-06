1808f3b83f821030d938bc799b745c8696c0c6a8
nilshen <linshen88@gmail.com>
2022-04-06 16:51:47 -0400

    w2d2 battleship

--- battleship_project/lib/battleship.rb
+++ battleship_project/lib/battleship.rb
@@ -2,5 +2,49 @@ require_relative "board"
 require_relative "player"
 
 class Battleship
+attr_reader :board, :player
 
+    def initialize(n)
+        @player = Player.new
+        @board = Board.new(n)
+        @remaining_misses = (n*n)/2
+    end
+
+    def start_game
+        @board.place_random_ships
+        # puts "#{@board.num_ships}" 
+        puts @board.num_ships
+        @board.print
+    end
+
+    def lose? 
+        if @remaining_misses <= 0 
+            puts "you lose"
+            return true
+        else false
+        end
+    end
+
+    def win? 
+        if @board.num_ships == 0
+            puts "you win"
+            return true
+        else false
+        end
+    end 
+    
+    def game_over? 
+        if self.win? || self.lose?
+            return true
+        end
+    false
+    end 
+
+    def turn
+      if !@board.attack(@player.get_move)
+        @remaining_misses -= 1
+      end
+      @board.print
+      puts "You have #{@remaining_misses} remaining misses"
+    end 
 end
--- battleship_project/lib/board.rb
+++ battleship_project/lib/board.rb
@@ -1,3 +1,82 @@
 class Board
-  
+    attr_reader (:size)
+
+    def self.print_grid(arr)
+        arr.each do |row|
+        puts row.join(" ")   
+        end
+    end
+
+    def initialize(n)
+        @num = n
+        @grid = Array.new(n){Array.new(n,:N)}
+        @size = n * n
+    end
+
+    def [](arr)
+        @grid[arr[0]][arr[1]]
+    end
+
+    def []=(arr,value)
+        @grid[arr[0]][arr[1]] = value
+    end
+
+    def num_ships
+        @grid.flatten.count(:S) #!!! count is only used for 1 dimension grid. So need to flatten first.
+    end
+
+    # def attack(pos)
+    #     if self[pos] == :S
+    #       self[pos] = :H
+    #       puts "you sunk my battleship!"
+    #       return true
+    #     else
+    #       self[pos] = :X
+    #       return false
+    #     end
+    #   end
+    
+    def attack(arr)
+        if self[arr] == :S # use syntactic sugar!!!! if self.[](arr) == :S 
+        self[arr] = :H                                 #self.[]=(arr, :H)
+        puts "you sunk my battleship!"  #!!!print will be duplicate with the print method on the bottom
+        return true
+        else 
+        self[arr] = :X                                 #self.[]=(arr, :X)
+        return false
+        end
+    end
+
+    def place_random_ships
+        # self.attack
+        # attack
+        n = (@size/4).to_i 
+        while self.num_ships < n
+        row = rand(1...@num)
+        col = rand(1...@num)
+        arr = [row,col]
+        self[arr] = :S                               #self.[]=(arr, :S)
+        end
+    end
+
+    def hidden_ships_grid
+     @grid.map do |row|
+            row.map do |el| 
+                if el == :S
+                    el = :N
+                else el
+                end
+            end
+        end 
+    end
+
+    def cheat
+        Board.print_grid(@grid)
+    end
+
+    def print
+        Board.print_grid(self.hidden_ships_grid)
+    end
+ 
+
 end
